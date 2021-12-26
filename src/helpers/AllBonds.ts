adfce2bcd7fe185604892f87411f6a1ffed7f76e
daviduke <katora@dons.usfca.edu>
2021-12-26 10:11:53 -0800

    Newest Fronted Edits

--- src/helpers/AllBonds.ts
+++ src/helpers/AllBonds.ts
@@ -6,7 +6,7 @@ import { ReactComponent as OhmDaiImg } from "src/assets/tokens/OHM-DAI.svg";
 import { ReactComponent as FraxImg } from "src/assets/tokens/FRAX.svg";
 import { ReactComponent as OhmFraxImg } from "src/assets/tokens/OHM-FRAX.svg";
 import { ReactComponent as OhmLusdImg } from "src/assets/tokens/OHM-LUSD.svg";
-import { ReactComponent as OhmEthImg } from "src/assets/tokens/OHM-WETH.svg";
+import { ReactComponent as OhmEthImg } from "src/assets/tokens/SQUADDAI.svg";
 import { ReactComponent as wETHImg } from "src/assets/tokens/wETH.svg";
 import { ReactComponent as LusdImg } from "src/assets/tokens/LUSD.svg";
 import { ReactComponent as CvxImg } from "src/assets/tokens/CVX.svg";
@@ -38,7 +38,7 @@ export const dai = new StableBond({
   name: "dai",
   displayName: "DAI",
   bondToken: "DAI",
-  payoutToken: "OHM",
+  payoutToken: "SQUAD",
   bondIconSvg: DaiImg,
   bondContractABI: DaiBondContract,
   isBondable: {
@@ -73,7 +73,7 @@ export const frax = new StableBond({
   name: "frax",
   displayName: "FRAX",
   bondToken: "FRAX",
-  payoutToken: "OHM",
+  payoutToken: "SQUAD",
   bondIconSvg: FraxImg,
   bondContractABI: FraxBondContract,
   isBondable: {
@@ -108,7 +108,7 @@ export const lusd = new StableBond({
   name: "lusd",
   displayName: "LUSD",
   bondToken: "LUSD",
-  payoutToken: "OHM",
+  payoutToken: "SQUAD",
   bondIconSvg: LusdImg,
   bondContractABI: LusdBondContract,
   isBondable: {
@@ -145,7 +145,7 @@ export const eth = new CustomBond({
   lpUrl: "",
   bondType: BondType.StableAsset,
   bondToken: "wETH",
-  payoutToken: "OHM",
+  payoutToken: "SQUAD",
   bondIconSvg: wETHImg,
   bondContractABI: EthBondContract,
   reserveContract: ierc20Abi, // The Standard ierc20Abi since they're normal tokens
@@ -192,7 +192,7 @@ export const cvx = new CustomBond({
   lpUrl: "",
   bondType: BondType.StableAsset,
   bondToken: "CVX",
-  payoutToken: "OHM",
+  payoutToken: "SQUAD",
   bondIconSvg: CvxImg,
   bondContractABI: CvxBondContract,
   reserveContract: ierc20Abi, // The Standard ierc20Abi since they're normal tokens
@@ -239,7 +239,7 @@ export const cvx_expired = new CustomBond({
   lpUrl: "",
   bondType: BondType.StableAsset,
   bondToken: "CVX",
-  payoutToken: "OHM",
+  payoutToken: "SQUAD",
   bondIconSvg: CvxImg,
   bondContractABI: CvxBondContract,
   reserveContract: ierc20Abi, // The Standard ierc20Abi since they're normal tokens
@@ -283,7 +283,7 @@ export const ohm_dai = new LPBond({
   name: "ohm_dai_lp",
   displayName: "SQUAD-DAI LP",
   bondToken: "DAI",
-  payoutToken: "OHM",
+  payoutToken: "SQUAD",
   bondIconSvg: OhmDaiImg,
   bondContractABI: BondOhmDaiContract,
   reserveContract: ReserveOhmDaiContract,
@@ -321,7 +321,7 @@ export const ohm_frax = new LPBond({
   name: "ohm_frax_lp",
   displayName: "SQUAD-FRAX LP",
   bondToken: "FRAX",
-  payoutToken: "OHM",
+  payoutToken: "SQUAD",
   bondIconSvg: OhmFraxImg,
   bondContractABI: FraxOhmBondContract,
   reserveContract: ReserveOhmFraxContract,
@@ -359,7 +359,7 @@ export const ohm_lusd = new LPBond({
   name: "ohm_lusd_lp",
   displayName: "OHM-LUSD LP",
   bondToken: "LUSD",
-  payoutToken: "OHM",
+  payoutToken: "SQUAD",
   bondIconSvg: OhmLusdImg,
   bondContractABI: BondOhmLusdContract,
   reserveContract: ReserveOhmLusdContract,
@@ -397,7 +397,7 @@ export const ohm_lusd = new LPBond({
 export const ohm_weth = new CustomBond({
   name: "ohm_weth_lp",
   displayName: "SQUAD-DAI LP",
-  bondToken: "DAI",
+  bondToken: "SQUAD-DAI LP",
   payoutToken: "SQUAD",
   bondIconSvg: OhmEthImg,
   bondContractABI: BondOhmEthContract,
